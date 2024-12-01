d87c149ebf4adbc7ef419eb15476c662ede3f352
Bastian Pedersen <bastian.tangedal@gmail.com>
2024-12-01 10:44:37 +0100

    Make proper `Shortcut` class and use translations

--- lib/features/home/widgets/home_shortcuts.dart
+++ lib/features/home/widgets/home_shortcuts.dart
@@ -1,3 +1,4 @@
+import 'package:evo/i18n/translations.g.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -6,20 +7,21 @@ class HomeShortcuts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> categories = [
-      {'icon': flashIcon, 'text': 'Dine nøkler'},
-      {'icon': fuck, 'text': 'Treningsøkter'},
+    List<Shortcut> shortcuts = [
+      Shortcut(svgIcon: keyIcon, title: context.t.homeScreen.shortcuts[0]),
+      Shortcut(svgIcon: workoutIcon, title: context.t.homeScreen.shortcuts[1]),
     ];
+
     return Padding(
       padding: const EdgeInsets.all(20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: List.generate(
-          categories.length,
-          (index) => CategoryCard(
-            icon: categories[index]['icon'],
-            text: categories[index]['text'],
+          shortcuts.length,
+          (index) => ShortcutCard(
+            icon: shortcuts[index].svgIcon,
+            text: shortcuts[index].title,
             press: () {},
           ),
         ),
@@ -28,11 +30,11 @@ class HomeShortcuts extends StatelessWidget {
   }
 }
 
-class CategoryCard extends StatelessWidget {
+class ShortcutCard extends StatelessWidget {
   final String icon, text;
   final GestureTapCallback press;
 
-  const CategoryCard({
+  const ShortcutCard({
     super.key,
     required this.icon,
     required this.text,
@@ -68,7 +70,17 @@ class CategoryCard extends StatelessWidget {
   }
 }
 
-const fuck =
+class Shortcut {
+  final String svgIcon;
+  final String title;
+
+  const Shortcut({
+    required this.svgIcon,
+    required this.title,
+  });
+}
+
+const workoutIcon =
     '''<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#C00080" class="size-6">
       <g fill="none" stroke="currentColor" stroke-width="1.5">
         <path d="M15.5 9v6c0 .465 0 .697.051.888a1.5 1.5 0 0 0 1.06 1.06c.191.052.424.052.889.052s.698 0 .888-.051a1.5 1.5 0 0 0 1.06-1.06c.052-.191.052-.424.052-.889V9c0-.465 0-.697-.051-.888a1.5 1.5 0 0 0-1.06-1.06C18.197 7 17.964 7 17.5 7s-.698 0-.888.051a1.5 1.5 0 0 0-1.06 1.06c-.052.192-.052.424-.052.889Zm-11 0v6c0 .465 0 .697.051.888a1.5 1.5 0 0 0 1.06 1.06c.192.052.424.052.889.052s.697 0 .888-.051a1.5 1.5 0 0 0 1.06-1.06c.052-.191.052-.424.052-.889V9c0-.465 0-.697-.051-.888a1.5 1.5 0 0 0-1.06-1.06C7.196 7 6.964 7 6.5 7s-.697 0-.888.051a1.5 1.5 0 0 0-1.06 1.06C4.5 8.304 4.5 8.536 4.5 9Z"/>
@@ -76,8 +88,8 @@ const fuck =
       </g>
     </svg>''';
 
-const flashIcon =
-    '''<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#C00080" class="size-6">
+const keyIcon =
+    '''<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
 </svg>
 ''';
