57c449793e363a4ff589c920643e315ed7a1768c
Bastian Pedersen <bastian.tangedal@gmail.com>
2024-12-01 21:24:21 +0100

    Add documentation to the method

--- lib/utils/formatting.dart
+++ lib/utils/formatting.dart
@@ -1,6 +1,23 @@
 import 'package:evo/i18n/translations.g.dart';
 import 'package:intl/intl.dart';
 
+/// Formats a [DateTime] object into a localized string.
+///
+/// - If the date is today, yesterday, or tomorrow, it returns a localized
+///   string like "Today", "Yesterday", or "Tomorrow" using the `slang` library.
+/// - If the date is within the current year, it formats as "MMMM d" (e.g., "December 3").
+/// - For dates in a different year, it formats as "MMMM d, y" (e.g., "December 3, 2023").
+///
+/// The formatting is localized based on the provided [locale] or the current
+/// locale from `slang`.
+///
+/// Parameters:
+/// - [dateTime]: The date to be formatted.
+/// - [locale]: Optional. The locale to use for formatting. Defaults to the
+///   current locale from `slang`.
+///
+/// Returns:
+/// - A localized string representation of the date.
 String formatDateTime(DateTime dateTime, {String? locale}) {
   final now = DateTime.now();
   final today = DateTime(now.year, now.month, now.day);
